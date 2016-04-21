04abd2f16752c7a29ee6c30f636fd74b7edaf22c
Dhamodharabalaji <dhamofficial@gmail.com>
2016-04-21 21:36:55 +0530

    ui improvements added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var myapp= angular.module('ngapp',['ngRoute']);
+var myapp= angular.module('ngapp',['ngRoute','ngSanitize']);
 
 
 myapp .config(['$routeProvider',  function($routeProvider) {
@@ -28,26 +28,41 @@ myapp.directive('tplServiceMgmt', ['myutilities',function (utiles) {
         templateUrl: 'templates/tplServiceManagement.html',
         scope: {
             module:'=',
+            shown:'='
         },
         controller: ['$scope','$http',function ($scope,$http) {
+            $scope.nav=function(mod){
 
-            $scope.Save=function(){
+            }
+            $scope.save=function(){
                 //call a webservice to save items
 
-                $http.get("http://localhost/so-enterprise-webapi/views/Index ")
-                    .then(function(response){ $scope.details = response.data; });
+                var form=$scope.Form;
+                $scope.list.push(form);
+                localStorage.setItem('__tickets',JSON.stringify($scope.list));
+                console.log('saved');
+                $scope.clear();
+                if(!$scope.createonemore)
+                $('#divmodal1').modal('toggle');
+                /*$http.post("http://localhost/so-enterprise-webapi/views/Index ")
+                    .then(function(response){ $scope.details = response.data; });*/
 
             }
 
             $scope.clear=function(){
-
+                $scope.Form={};
             }
 
             $scope.GetList=function()
             {
+                var list = localStorage.getItem('__tickets');
+                if(list && list.length>0)
+                    $scope.list = JSON.parse(list);
+
+
+
                 //to populate the list view
-                console.log('first invoke');
-                $http.get("http://localhost/so-enterprise-webapi/views/Index ")
+                /*$http.get("http://localhost/so-enterprise-webapi/views/Index ")
                 .then(function(response){
                     console.log('success');
                     $scope.details = response.data;
@@ -55,10 +70,43 @@ myapp.directive('tplServiceMgmt', ['myutilities',function (utiles) {
               ,function(){
               console.log('failure');
                 });
-                console.log('invoke end');
+                console.log('invoke end');*/
+            }
+            $scope.init=function(){
+                $scope.list=[];
+                $scope.clear();
+                $scope.GetList();
+                $scope.fn={save:$scope.save}
+            }
+            $scope.init();
+
+        }]
+    }
+}])
+
+.directive('tplServiceMgmtList', ['myutilities',function (utiles) {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/tplServiceManagementList.html',
+        scope: {
+            module:'=',
+            shown:'='
+        },
+        controller: ['$scope','$http',function ($scope,$http) {
+            $scope.nav=function(mod){
+
             }
 
-            //$scope.GetList();
+            $scope.GetList=function()
+            {
+                var list = localStorage.getItem('__tickets');
+                if(list && list.length>0)
+                    $scope.list = JSON.parse(list);
+            }
+            $scope.init=function(){
+                $scope.GetList();
+            }
+            $scope.init();
 
         }]
     }
@@ -108,81 +156,3 @@ myapp.controller('Controller1',['$scope',function(sc){
     }
 
 }]);
-(function(){
-        var $button = $("<div id='source-button' class='btn btn-floating-mini btn-primary'><i class='md  md-developer-mode'></i></div>").click(function(){
-          // Clean ripple
-          $(this).parent().find('.mtr-ripple-wrapper').remove();
-          $(this).parent().find('.mtr-btn').removeClass('mtr-btn');
-          var html = $(this).parent().html();
-          html = cleanSource(html);
-          $("#source-modal pre").text(html);
-          $("#source-modal").modal();
-        });
-
-        $('.bs-component [data-toggle="popover"]').popover();
-        $('.bs-component [data-toggle="tooltip"]').tooltip();
-
-        $(".bs-component").hover(function(){
-          $(this).append($button);
-          $button.show();
-        }, function(){
-          $button.hide();
-        });
-
-        function cleanSource(html) {
-          var lines = html.split(/\n/);
-
-          lines.shift();
-          lines.splice(-1, 1);
-
-          var indentSize = lines[0].length - lines[0].trim().length,
-              re = new RegExp(" {" + indentSize + "}");
-
-          lines = lines.map(function(line){
-            if (line.match(re)) {
-              line = line.substring(indentSize);
-            }
-
-            return line;
-          });
-
-          lines = lines.join("\n");
-
-          return lines;
-        }
-
-      })();
-
-$(function() {
-
-      $('.btn, .dropdown-menu a, .navbar a, .navbar-panel a, .toolbar a, .nav-pills a, .nav-tabs a, .pager a, .pagination a, .list-group a').mtrRipple({live: true}).on('click', function(e) {
-        e.preventDefault();
-      });
-
-      // Special case for checkbox / radio (no prevented event)
-      $('input[type=radio], input[type=checkbox]').mtrRipple({live: true});
-
-      // code snippet to add the valued class for input (for label positioning)
-      $('.form-control').on('blur input', function() {
-        var $this = $(this);
-        var v = $(this).val();
-        if (v && v != '') $this.addClass('valued');
-        else $this.removeClass('valued');
-      }).trigger('blur');
-
-      // Navbar panel
-      $('.navbar-panel').mtrPanel({toggle: '.toolbar .menu-toggle'});
-
-      $('#topbar').mtrHeader();
-
-      var $body = $('body');
-      var $headerTitle = $('.header-title');
-      $(window).on('scroll', function(e) {
-        var top = $body.scrollTop();
-        if (top > 275)
-          $headerTitle.addClass('small')
-        else
-          $headerTitle.removeClass('small');
-      });
-
-    });
